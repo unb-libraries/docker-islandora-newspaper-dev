6312a4e2821dec9113c2901bf3c289c34a3f1e39
Jacob Sanford <jsanford@unb.ca>
2015-12-15 08:12:06 -0400

    Changes related to VMWare docker hosting

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # A helper script for quickly tearing down the containers and rebuilding them.
+DOCKER_MACHINE_NAME=docker-vsphere-js
+LOCAL_DATA_SOURCE=/Users/jsanford/data/NBNDP
+DOCKER_DATA_TARGET=/home/docker
 
 docker pull mysql:5.6
 docker pull unblibraries/fedora-solr-gsearch:3.8.x
@@ -9,5 +12,6 @@ docker-compose rm
 cd drupal
 docker build .
 cd ..
-docker-compose build --no-cache
-docker-compose up
+docker-compose build
+# docker-machine scp -r ${LOCAL_DATA_SOURCE} ${DOCKER_MACHINE_NAME}:${DOCKER_DATA_TARGET}
+docker-compose up -d
